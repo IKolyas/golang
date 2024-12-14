65cd56f9b669983325d92b7a3bdc16fe7a4ef04f
IKolyas <i.kolyasnikov@mail.ru>
2024-12-14 19:03:38 +0500

    hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,30 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var result strings.Builder
+	var count int
+	for _, r := range s {
+		if r >= '0' && r <= '9' {
+			count = count*10 + int(r-'0')
+		} else {
+			if count == 0 {
+				count = 1
+			}
+			for i := 0; i < count; i++ {
+				result.WriteRune(r)
+			}
+			count = 0
+		}
+	}
+	if count != 0 {
+		return "", fmt.Errorf("invalid string")
+	}
+	return result.String(), nil
 }
