61e05d7662dc303767ad703e4ade229052872444
IKolyas <i.kolyasnikov@mail.ru>
2025-01-07 01:02:21 +0500

    hw 4

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,11 +13,48 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mutex    sync.Mutex
+}
+
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if item, ok := c.items[key]; ok {
+		item.Value = value
+		c.queue.MoveToFront(item)
+		return true
+	}
+
+	item := c.queue.PushFront(value)
+	c.items[key] = item
+
+	if c.queue.Len() > c.capacity {
+		last := c.queue.Back()
+		c.queue.Remove(last)
+		delete(c.items, key)
+	}
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		return item.Value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
 
 func NewCache(capacity int) Cache {
