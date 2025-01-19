1716b948b0fedab1d9294ef62e19a5c2a869cbdb
IKolyas <i.kolyasnikov@mail.ru>
2025-01-20 01:06:28 +0500

    fix

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -26,13 +26,11 @@ func Run(tasks []Task, n, m int) error {
 			defer wg.Done()
 			for task := range taskChan {
 				if err := task(); err != nil {
-					mu.Lock()
 					select {
 					case errorChan <- err:
 					case <-ctx.Done():
 						return
 					}
-					mu.Unlock()
 				}
 			}
 		}()
@@ -45,11 +43,11 @@ func Run(tasks []Task, n, m int) error {
 			case <-errorChan:
 				mu.Lock()
 				errorCount++
+				mu.Unlock()
 				if errorCount >= m {
 					cancel()
 					return
 				}
-				mu.Unlock()
 			case <-ctx.Done():
 				return
 			}
