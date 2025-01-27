ce666f257658faba883f3c27b3bba1b9035281d8
IKolyas <i.kolyasnikov@mail.ru>
2025-01-28 01:43:06 +0500

    hw 06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,14 +1,65 @@
 package hw06pipelineexecution
 
+import (
+	"sync"
+	"time"
+)
+
 type (
-	In  = <-chan interface{}
+	In = <-chan interface{}
+
 	Out = In
-	Bi  = chan interface{}
+
+	Bi = chan interface{}
 )
 
 type Stage func(in In) (out Out)
 
+func resAwait(in In, done In, out Bi) {
+	for {
+		select {
+		case <-done:
+			return
+		case value, ok := <-in:
+			if !ok {
+				return
+			}
+			out <- value
+		// Сливаем пайплайн по истечении отведённого времени
+		case <-time.After(time.Second):
+			return
+		}
+	}
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := make(chan interface{})
+
+	wg := sync.WaitGroup{}
+
+	for _, stage := range stages {
+		in = stage(in)
+	}
+
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		resAwait(in, done, out)
+	}()
+
+	go func() {
+		defer func() {
+			for range in {
+				// Сливаем зависшие вычисления
+				// Ругается линтер
+				<-in
+			}
+		}()
+
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
