c86b6c6c16914387a93a1d27db718e3d517bdda9
IKolyas <i.kolyasnikov@mail.ru>
2025-02-15 22:13:35 +0500

    HW08

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,26 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
 
-func TestReadDir(t *testing.T) {
-	// Place your code here
+func TestReadDirBinaryData(t *testing.T) {
+	tmpDir := t.TempDir()
+
+	filePath := filepath.Join(tmpDir, "BINARY")
+	err := os.WriteFile(filePath, []byte("\x00\x01\x02"), 0o644)
+	if err != nil {
+		t.Fatalf("Failed to create test file %s", err)
+	}
+
+	env, err := ReadDir(tmpDir)
+	if err != nil {
+		t.Fatalf("ReadDir failed: %v", err)
+	}
+
+	if _, exists := env["BINARY"]; exists {
+		t.Error("Key BINARY should have been ignored")
+	}
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,35 @@
 package main
 
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	command := exec.Command(cmd[0], cmd[1:]...)
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+
+	for key, envValue := range env {
+		if envValue.NeedRemove {
+			os.Unsetenv(key)
+		} else {
+			os.Setenv(key, envValue.Value)
+		}
+	}
+
+	command.Env = os.Environ()
+
+	if err := command.Run(); err != nil {
+		var exitError *exec.ExitError
+		if errors.As(err, &exitError) {
+			return exitError.ExitCode()
+		}
+		return 1
+	}
+
+	return 0
 }
