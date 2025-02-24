50085bb6496e713113d4749f80bbcdc0a08edec1
IKolyas <i.kolyasnikov@mail.ru>
2025-02-24 23:32:18 +0500

    hw09

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,197 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"slices"
+	"strconv"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
-	Err   error
+
+	Err error
 }
 
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	errStrings := make([]string, 0, len(v))
+	for _, err := range v {
+		errStrings = append(errStrings, fmt.Sprintf("%s: %s", err.Field, err.Err))
+	}
+	return strings.Join(errStrings, "; ")
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	val := reflect.ValueOf(v)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	if val.Kind() != reflect.Struct {
+		return errors.New("input is not a struct")
+	}
+
+	var validationErrors ValidationErrors
+
+	for i := range val.NumField() {
+		field := val.Type().Field(i)
+		fieldValue := val.Field(i)
+		validateTag := field.Tag.Get("validate")
+
+		if validateTag == "" {
+			continue
+		}
+
+		validators := strings.Split(validateTag, "|")
+
+		for _, validator := range validators {
+			err := applyValidator(fieldValue, validator)
+			if err != nil {
+				validationErrors = append(validationErrors, ValidationError{Field: field.Name, Err: err})
+			}
+		}
+	}
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
+
+	return nil
+}
+
+// applyValidator применяет конкретный валидатор к значению поля.
+
+func applyValidator(value reflect.Value, validator string) error {
+	parts := strings.SplitN(validator, ":", 2)
+
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid validator format: %s", validator)
+	}
+
+	rule := parts[0]
+	param := parts[1]
+
+	switch value.Kind() {
+	case reflect.String:
+		return validateString(value.String(), rule, param)
+	case reflect.Int:
+		return validateInt(int(value.Int()), rule, param)
+	case reflect.Slice:
+		return validateSlice(value, rule, param)
+	default:
+		return fmt.Errorf("unsupported type for validation: %s", value.Kind())
+	}
+}
+
+// validateString проверяет строку на соответствие правилам.
+
+func validateString(value, rule, param string) error {
+	switch rule {
+	case "len":
+		expectedLen, err := strconv.Atoi(param)
+		if err != nil {
+			return fmt.Errorf("invalid length parameter: %s", param)
+		}
+
+		if len([]rune(value)) != expectedLen {
+			return fmt.Errorf("length must be %d", expectedLen)
+		}
+	case "regexp":
+		re, err := regexp.Compile(param)
+		if err != nil {
+			return fmt.Errorf("invalid regexp: %s", param)
+		}
+
+		if !re.MatchString(value) {
+			return fmt.Errorf("must match regexp %s", param)
+		}
+	case "in":
+		allowedValues := strings.Split(param, ",")
+		if slices.Contains(allowedValues, value) {
+			return nil
+		}
+		return fmt.Errorf("must be one of %v", allowedValues)
+	default:
+		return fmt.Errorf("unknown validator for string: %s", rule)
+	}
+	return nil
+}
+
+// validateInt проверяет целое число на соответствие правилам.
+
+func validateInt(value int, rule, param string) error {
+	switch rule {
+	case "min":
+		minVal, err := strconv.Atoi(param)
+		if err != nil {
+			return fmt.Errorf("invalid min parameter: %s", param)
+		}
+
+		if value < minVal {
+			return fmt.Errorf("must be at least %d", minVal)
+		}
+
+	case "max":
+		maxVal, err := strconv.Atoi(param)
+		if err != nil {
+			return fmt.Errorf("invalid max parameter: %s", param)
+		}
+
+		if value > maxVal {
+			return fmt.Errorf("must be at most %d", maxVal)
+		}
+
+	case "in":
+		allowedValues := strings.Split(param, ",")
+
+		for _, allowed := range allowedValues {
+			allowedInt, err := strconv.Atoi(allowed)
+			if err != nil {
+				return fmt.Errorf("invalid int value in 'in' validator: %s", allowed)
+			}
+
+			if value == allowedInt {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("must be one of %v", allowedValues)
+	default:
+		return fmt.Errorf("unknown validator for int: %s", rule)
+	}
+
+	return nil
+}
+
+func validateSlice(value reflect.Value, rule, param string) error {
+	elemKind := value.Type().Elem().Kind()
+
+	for i := 0; i < value.Len(); i++ {
+		switch elemKind {
+		case reflect.String:
+
+			err := validateString(value.Index(i).String(), rule, param)
+			if err != nil {
+				return fmt.Errorf("element %d: %w", i, err)
+			}
+
+		case reflect.Int:
+
+			err := validateInt(int(value.Index(i).Int()), rule, param)
+			if err != nil {
+				return fmt.Errorf("element %d: %w", i, err)
+			}
+
+		default:
+
+			return fmt.Errorf("unsupported slice element type: %s", elemKind)
+		}
+	}
+
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -2,6 +2,7 @@ package hw09structvalidator
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -42,10 +43,53 @@ func TestValidate(t *testing.T) {
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:    "123456789012345678901234567890123456", // len:36
+				Age:   25,
+				Email: "test@example.com",
+				Role:  "admin",
+				Phones: []string{
+					"12345678901", // len:11
+					"09876543210", // len:11
+				},
+			},
+			expectedErr: nil,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: User{
+				ID:    "123",           // len:3 (invalid)
+				Age:   15,              // min:18 (invalid)
+				Email: "invalid-email", // regexp (invalid)
+				Role:  "user",          // in:admin,stuff (invalid)
+				Phones: []string{
+					"123", // len:3 (invalid)
+				},
+			},
+			expectedErr: ValidationErrors{
+				{Field: "ID", Err: fmt.Errorf("length must be 36")},
+				{Field: "Age", Err: fmt.Errorf("must be at least 18")},
+				{Field: "Email", Err: fmt.Errorf("must match regexp ^\\w+@\\w+\\.\\w+$")},
+				{Field: "Role", Err: fmt.Errorf("must be one of [admin stuff]")},
+				{Field: "Phones", Err: fmt.Errorf("element 0: length must be 11")},
+			},
+		},
+		{
+			in: App{
+				Version: "11.0.0", // len:5 (invalid)
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Version", Err: fmt.Errorf("length must be 5")},
+			},
+		},
+		{
+			in: Response{
+				Code: 400, // in:200,404,500 (invalid)
+				Body: "OK",
+			},
+			expectedErr: ValidationErrors{
+				{Field: "Code", Err: fmt.Errorf("must be one of [200 404 500]")},
+			},
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +97,40 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			err := Validate(tt.in)
+
+			if err == nil && tt.expectedErr == nil {
+				return
+			}
+
+			if err == nil || tt.expectedErr == nil {
+				t.Errorf("expected error: %v, got: %v", tt.expectedErr, err)
+				return
+			}
+
+			var expectedValidationErrors ValidationErrors
+			if !errors.As(tt.expectedErr, &expectedValidationErrors) {
+				t.Errorf("expected error type: ValidationErrors, got: %T", tt.expectedErr)
+				return
+			}
+
+			var actualValidationErrors ValidationErrors
+			if !errors.As(err, &actualValidationErrors) {
+				t.Errorf("expected error type: ValidationErrors, got: %T", err)
+				return
+			}
+
+			if len(expectedValidationErrors) != len(actualValidationErrors) {
+				t.Errorf("expected %d errors, got %d", len(expectedValidationErrors), len(actualValidationErrors))
+				return
+			}
+
+			for j := range expectedValidationErrors {
+				if expectedValidationErrors[j].Field != actualValidationErrors[j].Field ||
+					expectedValidationErrors[j].Err.Error() != actualValidationErrors[j].Err.Error() {
+					t.Errorf("expected error: %v, got: %v", expectedValidationErrors[j], actualValidationErrors[j])
+				}
+			}
 		})
 	}
 }
