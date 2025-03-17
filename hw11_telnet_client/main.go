8720439cd74e4e1c26359a87f4dd9c19e80b1c0e
IKolyas <i.kolyasnikov@mail.ru>
2025-03-18 00:51:30 +0500

    hw11

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	if len(os.Args) < 3 {
+		fmt.Fprintln(os.Stderr, "Usage: go-telnet [--timeout=<timeout>] <host> <port>")
+		os.Exit(1)
+	}
+	timeout := 10 * time.Second
+	args := os.Args[1:]
+	// Проверка аргументов на наличие таймаута
+	if len(args) > 0 && args[0][:10] == "--timeout=" {
+		var err error
+		timeout, err = time.ParseDuration(args[0][10:])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid timeout value: %v\n", err)
+			os.Exit(1)
+		}
+		args = args[1:]
+	}
+	if len(args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: go-telnet [--timeout=<timeout>] <host> <port>")
+		os.Exit(1)
+	}
+	address := fmt.Sprintf("%s:%s", args[0], args[1])
+	client := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
+	if err := client.Connect(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error connecting to server: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+	go func() {
+		if err := client.Send(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error sending data: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+	go func() {
+		if err := client.Receive(); err != nil {
+			fmt.Fprintf(os.Stderr, "Error receiving data: %v\n", err)
+			os.Exit(1)
+		}
+	}()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT)
+	<-sigChan
+	fmt.Fprintln(os.Stderr, "...SIGINT received, closing connection")
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -12,10 +17,73 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type TClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &TClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (tc *TClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", tc.address, tc.timeout)
+	if err != nil {
+		return err
+	}
+	tc.conn = conn
+	fmt.Fprintf(os.Stderr, "...Connected to %s\n", tc.address)
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (tc *TClient) Close() error {
+	if tc.conn != nil {
+		return tc.conn.Close()
+	}
+	return nil
+}
+
+func (tc *TClient) Send() error {
+	scanner := bufio.NewScanner(tc.in)
+	for scanner.Scan() {
+		_, err := tc.conn.Write(scanner.Bytes())
+		if err != nil {
+			return err
+		}
+		_, err = tc.conn.Write([]byte("\n"))
+		if err != nil {
+			return err
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+	fmt.Fprintln(os.Stderr, "...EOF")
+	return nil
+}
+
+func (tc *TClient) Receive() error {
+	reader := bufio.NewReader(tc.conn)
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+				return nil
+			}
+			return err
+		}
+		_, err = fmt.Fprint(tc.out, line)
+		if err != nil {
+			return err
+		}
+	}
+}
